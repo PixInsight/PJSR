55f3d5fa6a4fa9ab446793ede57e24b6ae3d86fb
Mike Schuster <mikeschuster@me.com>
2015-11-19 10:37:08 -0800

    MureDenoise Version 1.10

---
+++ src/scripts/mschuster/MureDenoise/MainModel.js
@@ -0,0 +1,401 @@
+// ****************************************************************************
+// PixInsight JavaScript Runtime API - PJSR Version 1.0
+// ****************************************************************************
+// MainModel.js - Released 2015/11/20 00:00:00 UTC
+// ****************************************************************************
+//
+// This file is part of MureDenoise Script Version 1.10
+//
+// Copyright (C) 2012-2015 Mike Schuster. All Rights Reserved.
+// Copyright (C) 2003-2015 Pleiades Astrophoto S.L. All Rights Reserved.
+//
+// Redistribution and use in both source and binary forms, with or without
+// modification, is permitted provided that the following conditions are met:
+//
+// 1. All redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. All redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the names "PixInsight" and "Pleiades Astrophoto", nor the names
+//    of their contributors, may be used to endorse or promote products derived
+//    from this software without specific prior written permission. For written
+//    permission, please contact info@pixinsight.com.
+//
+// 4. All products derived from this software, in any form whatsoever, must
+//    reproduce the following acknowledgment in the end-user documentation
+//    and/or other materials provided with the product:
+//
+//    "This product is based on software from the PixInsight project, developed
+//    by Pleiades Astrophoto and its contributors (http://pixinsight.com/)."
+//
+//    Alternatively, if that is where third-party acknowledgments normally
+//    appear, this acknowledgment must be reproduced in the product itself.
+//
+// THIS SOFTWARE IS PROVIDED BY PLEIADES ASTROPHOTO AND ITS CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PLEIADES ASTROPHOTO OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, BUSINESS
+// INTERRUPTION; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; AND LOSS OF USE,
+// DATA OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// ****************************************************************************
+
+function MainModel() {
+   // Image view.
+   this.imageViewFormat = "%s";
+   this.imageView = null;
+
+   // Minimum image size.
+   this.imageSizeMinimum = 256;
+
+   // Flatfield view.
+   this.flatfieldScaleFormat = "%.1f";
+   this.flatfieldScaleUnits = "%%";
+   this.flatfieldScaleNormalization = 100;
+   this.flatfieldViewFormat = "%s";
+   this.flatfieldView = null;
+
+   // Minimum flatfield size.
+   this.flatfieldSizeMinimum = 1024;
+
+   // Count of layers to remove for flatfield smoothing.
+   this.smoothFlatfieldLayerCount = 8;
+
+   // Combination count of the image.
+   this.imageCombinationCountMinimum = 1;
+   this.imageCombinationCountMaximum = 10000;
+   this.imageCombinationCountDefault = 1;
+   this.imageCombinationCountFormat = "%d";
+   this.imageCombinationCountUnits = "";
+   this.imageCombinationCount = this.imageCombinationCountDefault;
+
+   // Interpolation method names.
+   this.imageInterpolationMethodNames = [
+      "Nearest Neighbor",
+      "Bilinear",
+      "Bicubic Spline",
+      "Lanczos-3",
+      "Lanczos-4",
+      "Lanczos-5"
+   ];
+
+   // Noise correlation ratios.
+   this.noiseCorrelationRatios = [
+      1,       // Nearest Neighbor
+      3 / 2,   // Bilinear
+      70 / 57, // Bicubic Spline
+      1.11897, // Lancsoz-3
+      1.09407, // Lanczos-4
+      1.07036  // Lanczos-5
+   ];
+
+   // Noise variance scaling function.
+   this.noiseVarianceScaling = function() {
+      var v = this.denoiseVarianceScale;
+      var r = this.noiseCorrelationRatios[this.imageInterpolationMethod];
+      var n = this.imageCombinationCount;
+
+      return v * (1 + (n - 1) / (r * r)) / (n * n);
+   };
+
+   // Subband variance scaling function.
+   this.subbandVarianceScaling = function(level) {
+      var v = this.denoiseVarianceScale;
+      var n = this.imageCombinationCount;
+      var s = this.noiseVarianceScaling() / (v / n);
+      var a = -1 + Math.pow2(level) + Math.sqrt(s);
+      var b = -2 + Math.pow2(level) + 2 * Math.sqrt(s);
+      var ab = a / b;
+
+      return ab * ab;
+   };
+
+   // Interpolation method of the combination.
+   this.imageInterpolationMethodMinimum = 0;
+   this.imageInterpolationMethodMaximum =
+      this.imageInterpolationMethodNames.length - 1;
+   this.imageInterpolationMethodDefault = 3;
+   this.imageInterpolationMethodFormat = "%s";
+   this.imageInterpolationMethodUnits = "";
+   this.imageInterpolationMethod = this.imageInterpolationMethodDefault;
+
+   // Gain of the detector in e-/DN.
+   this.detectorGainMinimum = 0;
+   this.detectorGainMaximum = 65535;
+   this.detectorGainDefault = 0;
+   this.detectorGainFormat = "%.3f";
+   this.detectorGainUnits = "e-/DN";
+   this.detectorGain = this.detectorGainDefault;
+
+   // Effective gain.
+   this.effectiveGain = function() {
+      return this.detectorGain / this.noiseVarianceScaling();
+   };
+
+   // Standard deviation of additive white Gaussian noise of the detector in
+   // DN.
+   this.detectorGaussianNoiseMinimum = 0;
+   this.detectorGaussianNoiseMaximum = 65535;
+   this.detectorGaussianNoiseDefault = 0;
+   this.detectorGaussianNoiseFormat = "%.2f";
+   this.detectorGaussianNoiseUnits = "DN";
+   this.detectorGaussianNoise = this.detectorGaussianNoiseDefault;
+
+   // Effective standard deviation of additive white Gaussian noise.
+   this.effectiveGaussianNoise = function() {
+      return this.detectorGaussianNoise *
+         Math.sqrt(this.noiseVarianceScaling());
+   };
+
+   // Offset of the detector in DN.
+   this.detectorOffsetMinimum = 0;
+   this.detectorOffsetMaximum = 65535;
+   this.detectorOffsetDefault = 0;
+   this.detectorOffsetFormat = "%.1f";
+   this.detectorOffsetUnits = "DN";
+   this.detectorOffset = this.detectorOffsetDefault;
+
+   // Effective offset.
+   this.effectiveOffset = function() {
+      return this.detectorOffset;
+   };
+
+   // Denoise variance scale.
+   this.denoiseVarianceScaleMinimum = 0.1;
+   this.denoiseVarianceScaleMaximum = 10;
+   this.denoiseVarianceScaleDefault = 1;
+   this.denoiseVarianceScaleFormat = "%.3f";
+   this.denoiseVarianceScaleUnits = "";
+   this.denoiseVarianceScale = this.denoiseVarianceScaleDefault;
+
+   // Denoise cycle-spin count.
+   this.denoiseCycleSpinCountMinimum = 1;
+   this.denoiseCycleSpinCountMaximum = 32;
+   this.denoiseCycleSpinCountDefault = 8;
+   this.denoiseCycleSpinCountFormat = "%d";
+   this.denoiseCycleSpinCountUnits = "";
+   this.denoiseCycleSpinCount = this.denoiseCycleSpinCountDefault;
+
+   // Generate method noise image option.
+   this.generateMethodNoiseImageDefault = false;
+   this.generateMethodNoiseImageQuantileResolution = Math.round(Math.pow2(16));
+   this.generateMethodNoiseImageQuantileLow = 0.001;
+   this.generateMethodNoiseImageQuantileHigh = 0.999;
+   this.generateMethodNoiseImage = this.generateMethodNoiseImageDefault;
+
+   // Gives numeric if well defined and within range, otherwise a default.
+   this.defaultNumeric = function(numeric, min, max, def) {
+      return numeric != null &&
+         !isNaN(numeric) &&
+         numeric >= min && numeric <= max ? numeric : def;
+   };
+
+   // Gives boolean if well defined, otherwise a default.
+   this.defaultBoolean = function(bool, def) {
+      return bool != null && (bool == false || bool == true) ? bool : def;
+   };
+
+   // Loads core settings.
+   this.loadSettings = function() {
+      this.imageCombinationCount = this.defaultNumeric(
+         Settings.read("imageCombinationCount", DataType_Int32),
+         this.imageCombinationCountMinimum,
+         this.imageCombinationCountMaximum,
+         this.imageCombinationCountDefault
+      );
+      this.imageInterpolationMethod = this.defaultNumeric(
+         Settings.read("imageInterpolationMethod", DataType_Int32),
+         this.imageInterpolationMethodMinimum,
+         this.imageInterpolationMethodMaximum,
+         this.imageInterpolationMethodDefault
+      );
+
+      this.detectorGain = this.defaultNumeric(
+         Settings.read("detectorGain", DataType_Real32),
+         this.detectorGainMinimum,
+         this.detectorGainMaximum,
+         this.detectorGainDefault
+      );
+      this.detectorGaussianNoise = this.defaultNumeric(
+         Settings.read("detectorGaussianNoise", DataType_Real32),
+         this.detectorGaussianNoiseMinimum,
+         this.detectorGaussianNoiseMaximum,
+         this.detectorGaussianNoiseDefault
+      );
+      this.detectorOffset = this.defaultNumeric(
+         Settings.read("detectorOffset", DataType_Real32),
+         this.detectorOffsetMinimum,
+         this.detectorOffsetMaximum,
+         this.detectorOffsetDefault
+      );
+
+      this.denoiseVarianceScale = this.defaultNumeric(
+         Settings.read("denoiseVarianceScale", DataType_Real32),
+         this.denoiseVarianceScaleMinimum,
+         this.denoiseVarianceScaleMaximum,
+         this.denoiseVarianceScaleDefault
+      );
+      this.denoiseCycleSpinCount = this.defaultNumeric(
+         Settings.read("denoiseCycleSpinCount", DataType_Int32),
+         this.denoiseCycleSpinCountMinimum,
+         this.denoiseCycleSpinCountMaximum,
+         this.denoiseCycleSpinCountDefault
+      );
+      this.generateMethodNoiseImage = this.defaultBoolean(
+         Settings.read("generateMethodNoiseImage", DataType_Boolean),
+         this.generateMethodNoiseImageDefault
+      );
+   };
+
+   // Stores core settings.
+   this.storeSettings = function() {
+      Settings.write(
+         "imageCombinationCount",
+         DataType_Int32,
+         this.imageCombinationCount
+      );
+      Settings.write(
+         "imageInterpolationMethod",
+         DataType_Int32,
+         this.imageInterpolationMethod
+      );
+
+      Settings.write(
+         "detectorGain",
+         DataType_Real32,
+         this.detectorGain
+      );
+      Settings.write(
+         "detectorGaussianNoise",
+         DataType_Real32,
+         this.detectorGaussianNoise
+      );
+      Settings.write(
+         "detectorOffset",
+         DataType_Real32,
+         this.detectorOffset
+      );
+
+      Settings.write(
+         "denoiseVarianceScale",
+         DataType_Real32,
+         this.denoiseVarianceScale
+      );
+      Settings.write(
+         "denoiseCycleSpinCount",
+         DataType_Int32,
+         this.denoiseCycleSpinCount
+      );
+      Settings.write(
+         "generateMethodNoiseImage",
+         DataType_Boolean,
+         this.generateMethodNoiseImage
+      );
+   };
+
+   // Loads instance parameters.
+   this.loadParameters = function() {
+      if (Parameters.has("imageCombinationCount")) {
+         this.imageCombinationCount = this.defaultNumeric(
+            Parameters.getInteger("imageCombinationCount"),
+            this.imageCombinationCountMinimum,
+            this.imageCombinationCountMaximum,
+            this.imageCombinationCountDefault
+         );
+      }
+      if (Parameters.has("imageInterpolationMethod")) {
+         this.imageInterpolationMethod = this.defaultNumeric(
+            Parameters.getInteger("imageInterpolationMethod"),
+            this.imageInterpolationMethodMinimum,
+            this.imageInterpolationMethodMaximum,
+            this.imageInterpolationMethodDefault
+         );
+      }
+
+      if (Parameters.has("detectorGain")) {
+         this.detectorGain = this.defaultNumeric(
+            Parameters.getReal("detectorGain"),
+            this.detectorGainMinimum,
+            this.detectorGainMaximum,
+            this.detectorGainDefault
+         );
+      }
+      if (Parameters.has("detectorGaussianNoise")) {
+         this.detectorGaussianNoise = this.defaultNumeric(
+            Parameters.getReal("detectorGaussianNoise"),
+            this.detectorGaussianNoiseMinimum,
+            this.detectorGaussianNoiseMaximum,
+            this.detectorGaussianNoiseDefault
+         );
+      }
+      if (Parameters.has("detectorOffset")) {
+         this.detectorOffset = this.defaultNumeric(
+            Parameters.getReal("detectorOffset"),
+            this.detectorOffsetMinimum,
+            this.detectorOffsetMaximum,
+            this.detectorOffsetDefault
+         );
+      }
+
+      if (Parameters.has("denoiseVarianceScale")) {
+         this.denoiseVarianceScale = this.defaultNumeric(
+            Parameters.getReal("denoiseVarianceScale"),
+            this.denoiseVarianceScaleMinimum,
+            this.denoiseVarianceScaleMaximum,
+            this.denoiseVarianceScaleDefault
+         );
+      }
+      if (Parameters.has("denoiseCycleSpinCount")) {
+         this.denoiseCycleSpinCount = this.defaultNumeric(
+            Parameters.getInteger("denoiseCycleSpinCount"),
+            this.denoiseCycleSpinCountMinimum,
+            this.denoiseCycleSpinCountMaximum,
+            this.denoiseCycleSpinCountDefault
+         );
+      }
+      if (Parameters.has("generateMethodNoiseImage")) {
+         this.generateMethodNoiseImage = this.defaultBoolean(
+            Parameters.getBoolean("generateMethodNoiseImage"),
+            this.generateMethodNoiseImageDefault
+         );
+      }
+   };
+
+   // Stores instance parameters.
+   this.storeParameters = function() {
+      Parameters.clear();
+
+      Parameters.set("version", VERSION);
+
+      Parameters.set("imageCombinationCount", this.imageCombinationCount);
+      Parameters.set(
+         "imageInterpolationMethod", this.imageInterpolationMethod
+      );
+
+      Parameters.set("detectorGain", this.detectorGain);
+      Parameters.set("detectorGaussianNoise", this.detectorGaussianNoise);
+      Parameters.set("detectorOffset", this.detectorOffset);
+
+      Parameters.set("denoiseVarianceScale", this.denoiseVarianceScale);
+      Parameters.set("denoiseCycleSpinCount", this.denoiseCycleSpinCount);
+      Parameters.set(
+         "generateMethodNoiseImage", this.generateMethodNoiseImage
+      );
+   };
+
+   // Clears the model.
+   this.clear = function() {
+      this.imageView = null;
+      this.flatfieldView = null;
+   };
+}
+
+// ****************************************************************************
+// EOF MainModel.js - Released 2015/11/20 00:00:00 UTC
---
+++ src/scripts/mschuster/MureDenoise/MureDenoise.js
@@ -0,0 +1,154 @@
+// ****************************************************************************
+// PixInsight JavaScript Runtime API - PJSR Version 1.0
+// ****************************************************************************
+// MureDenoise.js - Released 2015/11/20 00:00:00 UTC
+// ****************************************************************************
+//
+// This file is part of MureDenoise Script Version 1.10
+//
+// Copyright (C) 2012-2015 Mike Schuster. All Rights Reserved.
+// Copyright (C) 2003-2015 Pleiades Astrophoto S.L. All Rights Reserved.
+//
+// Redistribution and use in both source and binary forms, with or without
+// modification, is permitted provided that the following conditions are met:
+//
+// 1. All redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. All redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the names "PixInsight" and "Pleiades Astrophoto", nor the names
+//    of their contributors, may be used to endorse or promote products derived
+//    from this software without specific prior written permission. For written
+//    permission, please contact info@pixinsight.com.
+//
+// 4. All products derived from this software, in any form whatsoever, must
+//    reproduce the following acknowledgment in the end-user documentation
+//    and/or other materials provided with the product:
+//
+//    "This product is based on software from the PixInsight project, developed
+//    by Pleiades Astrophoto and its contributors (http://pixinsight.com/)."
+//
+//    Alternatively, if that is where third-party acknowledgments normally
+//    appear, this acknowledgment must be reproduced in the product itself.
+//
+// THIS SOFTWARE IS PROVIDED BY PLEIADES ASTROPHOTO AND ITS CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PLEIADES ASTROPHOTO OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, BUSINESS
+// INTERRUPTION; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; AND LOSS OF USE,
+// DATA OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// ****************************************************************************
+
+#define TITLE "MureDenoise"
+#define VERSION "1.10"
+
+#feature-id Noise Reduction > MureDenoise
+
+#feature-info <b>MureDenoise Version 1.10</b><br/>\
+   <br/>\
+   Script for denoising linear monochannel images corrupted by mixed \
+   Poisson-Gaussian noise. Applicable to single frame images and average \
+   combinations of equally exposed and registered frames.<br/>\
+   <br/>\
+   The script supports an astronomical image processing workflow in which \
+   the denoising step occurs immediately after the calibration and optional \
+   average combination steps and prior to other linear or nonlinear \
+   processing steps.<br/>\
+   <br/>\
+   The script applies an interscale wavelet mixed noise unbiased risk \
+   estimator (MURE) to find a denoised output image that minimizes an \
+   estimate of the oracle mean-squared error (MSE), or &quot;risk&quot;, \
+   between the denoised output image and the unknown noise-free image.<br/>\
+   <br/>\
+   <b>Note</b>: For linear multichannel images, run the monochannel denoiser \
+   on each channel separately. For linear one shot color (OSC) images, \
+   denoise the color filter array (CFA) channels not the debayered RGB \
+   channels.<br/>\
+   <br/>\
+   <b>Warning</b>: The script is adapted to denoise linear monochannel images \
+   mainly corrupted by shot noise, read noise, and dark current noise which \
+   is typically the case for astronomical data. The script does not work \
+   properly for other noise distributions, for saturated images, for linearly \
+   or nonlinearly processed images, for median combinations, or for drizzle \
+   combinations.<br/>\
+   <br/>\
+   <b>Warning</b>: Do not combine denoised images. Signal-to-noise ratio \
+   (SNR) will be enhanced by combining noisy images and denoising the \
+   result. Combined images must be equally exposed, have the same pixel \
+   resolution, and be registered by projective transformation with no \
+   distortion correction. When denoising combinations, the script is unable \
+   to remove correlated noise introduced by the image registration \
+   process.<br/>\
+   <br/>\
+   Copyright &copy; 2012-2015 Mike Schuster. All Rights Reserved.<br/>\
+   Copyright &copy; 2003-2015 Pleiades Astrophoto S.L. All Rights Reserved.
+
+#include <pjsr/ColorSpace.jsh>
+#include <pjsr/DataType.jsh>
+#include <pjsr/FontFamily.jsh>
+#include <pjsr/FrameStyle.jsh>
+#include <pjsr/PenStyle.jsh>
+#include <pjsr/SampleType.jsh>
+#include <pjsr/Sizer.jsh>
+#include <pjsr/StdButton.jsh>
+#include <pjsr/StdIcon.jsh>
+#include <pjsr/TextAlign.jsh>
+#include <pjsr/UndoFlag.jsh>
+
+#include "FrameMatrix.js"
+#include "Global.js"
+#include "MainModel.js"
+#include "MainViewController.js"
+#include "MureEstimator.js"
+#include "NR3SVD.js"
+
+function main() {
+   console.hide();
+
+   var model = new MainModel();
+   model.loadSettings();
+   model.loadParameters();
+
+   var controller = new MainController(model);
+
+   var view = new MainView(model, controller);
+   controller.setView(view);
+
+   if (Parameters.isViewTarget) {
+      controller.setImageView(Parameters.targetView);
+   }
+   else {
+      controller.setImageView(ImageWindow.activeWindow.currentView);
+   }
+
+   for (;;) {
+      controller.execute();
+      if (true || (new MessageBox(
+         "Do you really want to dismiss " + TITLE + "?",
+         TITLE,
+         StdIcon_Question,
+         StdButton_No,
+         StdButton_Yes
+      )).execute() == StdButton_Yes) {
+         break;
+      }
+   }
+
+   model.storeSettings();
+   model.clear();
+}
+
+main();
+
+gc();
+
+// ****************************************************************************
+// EOF MureDenoise.js - Released 2015/11/20 00:00:00 UTC
