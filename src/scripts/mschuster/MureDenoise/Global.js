fa697c6c7b595af77437a89de0aad2e5a74344f4
Mike Schuster <mikeschuster@me.com>
2016-02-22 14:09:42 -0800

    MureDenoise 1.15

--- src/scripts/mschuster/MureDenoise/Global.js
+++ src/scripts/mschuster/MureDenoise/Global.js
@@ -1,13 +1,13 @@
 // ****************************************************************************
 // PixInsight JavaScript Runtime API - PJSR Version 1.0
 // ****************************************************************************
-// Global.js - Released 2015/12/04 00:00:00 UTC
+// Global.js - Released 2016/02/24 00:00:00 UTC
 // ****************************************************************************
 //
-// This file is part of MureDenoise Script Version 1.14
+// This file is part of MureDenoise Script Version 1.15
 //
-// Copyright (C) 2012-2015 Mike Schuster. All Rights Reserved.
-// Copyright (C) 2003-2015 Pleiades Astrophoto S.L. All Rights Reserved.
+// Copyright (C) 2012-2016 Mike Schuster. All Rights Reserved.
+// Copyright (C) 2003-2016 Pleiades Astrophoto S.L. All Rights Reserved.
 //
 // Redistribution and use in both source and binary forms, with or without
 // modification, is permitted provided that the following conditions are met:
@@ -99,4 +99,4 @@ if (!Sizer.prototype.addUnscaledSpacing) {
 }
 
 // ****************************************************************************
-// EOF Global.js - Released 2015/12/04 00:00:00 UTC
+// EOF Global.js - Released 2016/02/24 00:00:00 UTC
