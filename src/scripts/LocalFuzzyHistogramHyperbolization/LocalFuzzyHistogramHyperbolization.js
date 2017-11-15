45c91b399939213052a936266f9e07f9f19dd4c9
jconejero <juan.conejero@pixinsight.com>
2017-11-15 09:56:28 +0100

    LocalFuzzyHistogramHyperbolization: Update to version 1.02

--- src/scripts/LocalFuzzyHistogramHyperbolization/LocalFuzzyHistogramHyperbolization.js
+++ src/scripts/LocalFuzzyHistogramHyperbolization/LocalFuzzyHistogramHyperbolization.js
@@ -1,5 +1,53 @@
+// ----------------------------------------------------------------------------
+// PixInsight JavaScript Runtime API - PJSR Version 1.0
+// ----------------------------------------------------------------------------
+// LocalFuzzyHistogramHyperbolization.js - Released 2017-11-15T08:54:44Z
+// ----------------------------------------------------------------------------
+//
+// This file is part of LocalFuzzyHistogramHyperbolization Script version 1.02
+//
+// Copyright (c) 2017 Frank Weidenbusch
+//
+// Redistribution and use in both source and binary forms, with or without
+// modification, is permitted provided that the following conditions are met:
+//
+// 1. All redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//
+// 2. All redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the names "PixInsight" and "Pleiades Astrophoto", nor the names
+//    of their contributors, may be used to endorse or promote products derived
+//    from this software without specific prior written permission. For written
+//    permission, please contact info@pixinsight.com.
+//
+// 4. All products derived from this software, in any form whatsoever, must
+//    reproduce the following acknowledgment in the end-user documentation
+//    and/or other materials provided with the product:
+//
+//    "This product is based on software from the PixInsight project, developed
+//    by Pleiades Astrophoto and its contributors (http://pixinsight.com/)."
+//
+//    Alternatively, if that is where third-party acknowledgments normally
+//    appear, this acknowledgment must be reproduced in the product itself.
+//
+// THIS SOFTWARE IS PROVIDED BY PLEIADES ASTROPHOTO AND ITS CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL PLEIADES ASTROPHOTO OR ITS
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, BUSINESS
+// INTERRUPTION; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; AND LOSS OF USE,
+// DATA OR PROFITS) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// ----------------------------------------------------------------------------
+
 #define TITLE "LocalFuzzyHistogramHyperbolization Script"
-#define VERSION "1.01"
+#define VERSION "1.02"
 
 #feature-id Utilities > LocalFuzzyHistogramHyperbolization
 #feature-info  A script for contrast enhancement
@@ -1132,7 +1180,7 @@ function parametersDialogPrototype() {
 //===== Define Segmentation =====
 
    this.segmentation_Sizer = new HorizontalSizer;
-   this.segmentation_Sizer.spacing = 6;
+   this.segmentation_Sizer.spacing = 4;
 
    this.segmentation_Label = new Label(this);
    this.segmentation_Label.setFixedWidth(alignmentWidth);
@@ -1455,7 +1503,7 @@ function parametersDialogPrototype() {
 //===== Define Fuzzy Membership Function =====
 
    this.algorithm_Sizer = new HorizontalSizer;
-   this.algorithm_Sizer.spacing = 6;
+   this.algorithm_Sizer.spacing = 4;
 
    this.algorithm_Label = new Label(this);
    this.algorithm_Label.setFixedWidth(alignmentWidth);
@@ -1849,3 +1897,6 @@ function main() {
 }
 
 main();
+
+// ----------------------------------------------------------------------------
+// EOF LocalFuzzyHistogramHyperbolization.js - Released 2017-11-15T08:54:44Z
